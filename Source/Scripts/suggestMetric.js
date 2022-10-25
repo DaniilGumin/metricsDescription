963d15e31b8d1a9415b196a1e1e3ce368ce82e75
DaniilGumin <gumin.danya@gmail.com>
2022-10-25 19:29:43 +0300

    fix: add validation in input

--- Source/Scripts/suggestMetric.js
+++ Source/Scripts/suggestMetric.js
@@ -3,13 +3,21 @@ function listenToSubmit() {
         event.preventDefault()
         const metricName = document.getElementById("metric-name").value
         const metricDescription = document.getElementById("metric-description").value
-        alert(metricDescription)
         const contact = document.getElementById("contact").value
 
         if (metricName.length === 0 || metricDescription.length === 0 || contact.length === 0) {
             alert("Все поля должны быть заполнены.")
             return
         }
+
+        if (/<\/?[a-z][\s\S]*>/i.test(metricName) || /<\/?[a-z][\s\S]*>/i.test(metricDescription) || /<\/?[a-z][\s\S]*>/i.test(contact)) {
+            alert("Вы плохой человек и пытались ввести строку содержащую HTML.")
+            return
+        }
+
+        document.getElementById("metric-name").value = "";
+        document.getElementById("metric-description").value = "";
+        document.getElementById("contact").value = "";
 
         const hash = new Date().getTime() + metricName
         const suggestObject = {"name": metricName, "description": metricDescription, "user-contact": contact}
@@ -22,7 +30,7 @@ function displayMetrics() {
     const keys = Object.keys(localStorage)
     let count_of_suggest = keys.length
 
-    while ( count_of_suggest-- ) {
+    while (count_of_suggest--) {
         renderSuggest(JSON.parse(localStorage.getItem(keys[count_of_suggest])))
     }
 }
@@ -31,20 +39,20 @@ function renderSuggest(suggestObject) {
     const rowRender = `
       <tr class="suggest-metric-row">
           <td class="metric-suggest-field-with-right-line">
-              ${suggestObject["name"]}
+              ${suggestObject["name"].toString()}
           </td>
           <td class="metric-suggest-field-with-right-line">
-              <pre>${suggestObject["description"]}</pre>
+              <pre>${suggestObject["description"].toString()}</pre>
           </td>
           <td class="metric-suggest-field">
-              ${suggestObject["user-contact"]}
+              ${suggestObject["user-contact"].toString()}
           </td>
       </tr>
     `;
     document.querySelector(".suggest-metric").insertAdjacentHTML("beforeend", rowRender);
 }
 
-(function() {
+(function () {
     document.addEventListener("DOMContentLoaded", () => {
         listenToSubmit()
         displayMetrics()
